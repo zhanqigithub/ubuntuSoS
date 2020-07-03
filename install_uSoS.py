95de31f76f41e594150ca612b3c7145b222f4349
zhanqi <sherry.qi.zhang@intel.com>
2020-07-03 16:43:31 +0800

    seperate kernel and acrn-hypervisor config

--- install_uSoS.py
+++ install_uSoS.py
@@ -4,12 +4,6 @@ import subprocess
 import datetime
 import time
 
-from USoS_lib import install_compile_package
-from USoS_lib import create_acrn_kernel_deb
-from USoS_lib import install_acrn_deb
-from USoS_lib import install_acrn_kernel_deb
-from USoS_lib import build_acrn_kernel
-
 #parse json file
 with open("release.json","r") as load_f:
 	load_dict = json.load(load_f)
@@ -19,22 +13,55 @@ with open("deb.json","r") as load_fdeb:
 	load_dictdeb = json.load(load_fdeb)
 load_fdeb.close()
 
+
+# install compile package
+def install_compile_package():
+	#check compile env
+	os.system('apt install gcc \
+     git \
+     make \
+     gnu-efi \
+     libssl-dev \
+     libpciaccess-dev \
+     uuid-dev \
+     libsystemd-dev \
+     libevent-dev \
+     libxml2-dev \
+     libusb-1.0-0-dev \
+     python3 \
+     python3-pip \
+     libblkid-dev \
+     e2fslibs-dev \
+     pkg-config \
+     libelf-dev\
+     libnuma-dev -y')
+	gcc_version_out = os.popen('gcc --version')
+	gcc_version = gcc_version_out.read()
+	if tuple(gcc_version.split(' ')[3].split("\n")[0].split(".")) < tuple(load_dict['gcc_version'].split('.')):
+		print("your gcc version is too old")
+	binutils_version_out = os.popen('ld -v')
+	binutils_version = binutils_version_out.read().split(" ")
+	if tuple(binutils_version[-1].split("\n")[0].split(".")) < tuple(load_dict['binutils'].split('.')):
+		print("your binutils version is too old")
+	os.system('apt install python-pip -y')
+	os.system('pip3 install kconfiglib')
+	os.system('apt-get install bison -y')
+	os.system('apt-get install flex -y')
+	os.system('apt install liblz4-tool -y')
+
+
 # build acrn
 def build_acrn():
-	install_compile_package()
 	if os.path.exists('acrn_release_img'):
 		os.system('rm -rf acrn_release_img')
 	os.system('mkdir -p acrn_release_img')
-	cmd = 'git clone %s' % load_dict['acrn_repo']
-	os.system(cmd)
 
-	if not os.path.exists('acrn-hypervisor'):
-		#os.system('rm -rf acrn-hypervisor')
-
+	if load_dict['sync_acrn_code'] == 'true':
+		os.system('rm -rf acrn-hypervisor')
 		cmd = 'git clone %s' % load_dict['acrn_repo']
 		os.system(cmd)
-	cmd = 'cd acrn-hypervisor' + "&&" +'git checkout -b mybranch %s'% load_dict['release_version']
-	os.system(cmd)
+		cmd = 'cd acrn-hypervisor' + "&&" +'git checkout -b mybranch %s'% load_dict['release_version']
+		os.system(cmd)
 
 	# industry
 
@@ -74,9 +101,89 @@ def build_acrn():
 				cmd = 'cp %s acrn_release_img/%s' %(load_dictdeb['acrn.efi']['source'],efi_name)
 				os.system(cmd)
 
-	build_acrn_kernel(load_dict['sos_kernel_repo'],load_dict['release_version'])
+def create_acrn_kernel_deb():
 
-def create_release_deb():
+	if os.path.exists('acrn_kernel_deb'):
+		os.system('rm -rf acrn_kernel_deb')
+
+	os.system('mkdir -p acrn_kernel_deb')
+
+	cmd = "cd acrn_kernel_deb" + "&&" +"mkdir DEBIAN"
+	os.system(cmd)
+
+	cmd = "cd acrn_kernel_deb" + "&&" +"touch DEBIAN/control"
+	os.system(cmd)
+
+	#control file description
+
+	listcontrol=['Package: acrn-kernel-package\n','version: %s \n'% datetime.date.today(),'Section: free \n','Priority: optional \n','Architecture: amd64 \n','Installed-Size: 66666 \n','Maintainer: Intel\n','Description: sos_kernel \n','\n']
+
+
+	with open('acrn_kernel_deb/DEBIAN/control','w',encoding='utf-8') as fr:
+			fr.writelines(listcontrol)
+
+	cmd = 'cd acrn-kernel' + '&&' + 'ls *.gz'
+	filename = os.popen(cmd).read().replace('\n', '').replace('\r', '')
+	cmd = 'cp acrn-kernel/%s acrn_kernel_deb/' % filename
+	os.system(cmd)
+
+	cmd = 'cd acrn_kernel_deb' + '&&' + 'tar -zvxf %s' % filename
+	os.system(cmd)
+
+
+	cmd = 'cd acrn_kernel_deb/boot' + '&&' + 'ls vmlinuz*'
+	version = os.popen(cmd)
+
+	f = open("acrn_kernel_deb/boot/version.txt",'w')
+	f.write(version.read())
+	f.close()
+
+	os.system('cp acrn-kernel.postinst acrn_kernel_deb/DEBIAN/postinst' )
+
+	os.system('chmod +x acrn_kernel_deb/DEBIAN/postinst')
+
+	os.system('sed -i \'s/\r//\' acrn_kernel_deb/DEBIAN/postinst')
+
+	os.system('rm acrn_kernel_deb/%s' % filename)
+
+	os.system('dpkg -b acrn_kernel_deb acrn_kernel_deb_package.deb ')
+
+
+def build_acrn_kernel(acrn_repo,acrn_version):
+	if load_dict['sync_acrn_kernel_code'] == 'true':
+
+		os.system('git clone %s' % acrn_repo)
+
+		cmd = 'cd acrn-kernel' + "&&" +'git checkout %s'% acrn_version
+		os.system(cmd)
+
+	cmd = 'cd acrn-kernel' + "&&" +'make clean'
+	os.system(cmd)
+	# build kernel
+	cmd = 'cd acrn-kernel' + "&&" +'cp kernel_config_uefi_sos .config'
+	os.system(cmd)
+
+	cmd = 'cd acrn-kernel' + "&&" +'make olddefconfig'
+	os.system(cmd)
+
+	cmd = 'cd acrn-kernel' + "&&" +'make targz-pkg -j4'
+	os.system(cmd)
+
+
+
+#install acrn
+def install_acrn_deb():
+	os.system('dpkg -r acrn-package')
+	os.system('dpkg -i acrn_deb_package.deb ')
+
+
+#install kernel
+def install_acrn_kernel_deb():
+	os.system('dpkg -r acrn-kernel-package ')
+	os.system('dpkg -i acrn_kernel_deb_package.deb ')
+
+
+def create_acrn_deb():
 
 	path = 'acrn_release_deb'
 	if os.path.exists(path):
@@ -131,21 +238,38 @@ def create_release_deb():
 
 	os.system('dpkg -b acrn_release_deb acrn_deb_package.deb ')
 
-	create_acrn_kernel_deb()
 
 
 def install_process():
+
+	if load_dict['install_package'] == 'true':
+		print('start install package')
+		install_compile_package()
+
 	if load_dict['build_acrn'] == 'true':
 		print('start build acrn')
 		build_acrn()
 
-	if load_dict['create_acrn_deb'] == 'true':
-		print('start create acrn release deb')
-		create_release_deb()
+	if load_dict['build_acrn_kernel'] == 'true':
+		print('start build_acrn_kernel')
+		build_acrn_kernel(load_dict['sos_kernel_repo'],load_dict['kernel_release_version'])
+
+	if load_dict['acrn_deb_package'] == 'true':
+		print('start create acrn_deb_package deb')
+		create_acrn_deb()
+
+	if load_dict['acrn_kernel_deb_package'] == 'true':
+		print('start create acrn_kernel_deb_package deb')
+		create_acrn_kernel_deb()
 
 	if load_dict['install_acrn_deb'] == 'true':
-		print('start install acrn release deb')
+		print('start install_acrn_deb')
 		install_acrn_deb()
+
+
+	if load_dict['install_acrn_kernel_deb'] == 'true':
+		print('start install install_acrn_kernel_deb')
 		install_acrn_kernel_deb()
+
 if __name__ == "__main__":
 		install_process()
